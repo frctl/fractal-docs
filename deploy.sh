a8546dd2f4d408d4b59c290b996c355dc328540c
samuelgoddard <hello@samgoddard.co.uk>
2018-09-05 12:48:31 +0100

    removed unneccesary parts of deploy script

--- deploy.sh
+++ deploy.sh
@@ -16,9 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:samuelgoddard/fractal-docs.git master:gh-pages
 
